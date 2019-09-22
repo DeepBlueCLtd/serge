fbbc2c85f6722c1ab19deee8e035783b91682b10
Heri Setiawan <heri.dehero@gmail.com>
2019-09-22 13:23:26 +0700

    Add wargame overview tests

--- client/src/Tests/demoScreen.test.js
+++ client/src/Tests/demoScreen.test.js
@@ -28,13 +28,18 @@ beforeAll(async () => {
 
 describe('Demo screen admin interface', () => {
   test('Login to game setup', async () => {
-    const anchor = '#form-login-admin';
     let pageTitle;
+    const anchor = '#form-login-admin';
+    const selectors = {
+      password: `${anchor} [type=password]`,
+      submit: `${anchor} .link`,
+    };
     await page.goto('localhost:8080/serge/demo');
-    await page.waitForSelector(`${anchor} [type=password]`);
-    await page.type(`${anchor} [type=password]`, DEFAULT_SERVER); // cannot import consts file as import statements
-    await page.click(`${anchor} .link`);
-    await page.waitFor('#page-title');
+    await page.waitForSelector(selectors.password);
+    await page.click(selectors.password);
+    await page.type(selectors.password, DEFAULT_SERVER);
+    await page.click(selectors.submit);
+    await page.waitForSelector('#page-title');
 
     pageTitle = await page.evaluate(() => document.querySelector('#page-title').innerText);
     expect(pageTitle).toEqual('Games');
@@ -42,11 +47,47 @@ describe('Demo screen admin interface', () => {
 
   test('Create wargame', async () => {
     let wargameName;
-    await page.waitForSelector('.game-designer-action .link:nth-of-type(1)');
-    await page.click('.game-designer-action .link:nth-of-type(1)');
-    await page.waitFor('#game-setup-head');
+    const selectors = {
+      createWargame: '.game-designer-action .link:nth-of-type(1)',
+    };
+    await page.waitForSelector(selectors.createWargame);
+    await page.click(selectors.createWargame);
+    await page.waitForSelector('#game-setup-head');
 
     wargameName = await page.evaluate(() => document.querySelector('#game-setup-head').length);
     expect(wargameName).not.toBeNull();
+  }, 15000);
+
+  test('Modify wargame overview description', async () => {
+    let wargameDesc;
+    const anchor = '#game-setup-tab-settings';
+    const selectors = {
+      gameDescription: '[name=wargame-overview-desc]',
+      saveOverview: `${anchor} [data-qa-type=submit]`,
+    };
+    await page.waitForSelector(selectors.gameDescription);
+    await page.click(selectors.gameDescription);
+    await page.type(selectors.gameDescription, 'Game overview example');
+    await page.waitForSelector(selectors.saveOverview);
+    await page.click(selectors.saveOverview);
+
+    wargameDesc = await page.evaluate(selectors => document.querySelector(selectors.gameDescription).value, selectors);
+    expect(wargameDesc).toBe('Game overview example');
+  }, 15000);
+
+  test('Enable access code', async () => {
+    let accessCode;
+    const anchor = '#game-setup-tab-settings';
+    const selectors = {
+      accessCode: '#show-access-codes',
+      saveOverview: `${anchor} [data-qa-type=submit]`,
+    };
+    await page.waitForSelector(selectors.accessCode);
+    await page.click(selectors.accessCode);
+    await page.waitForSelector(selectors.saveOverview);
+    await page.click(selectors.saveOverview);
+
+    accessCode = await page.evaluate(selectors => document.querySelector(selectors.accessCode).checked, selectors);
+    expect(accessCode).toBeTruthy();
   }, 15000);
 });
